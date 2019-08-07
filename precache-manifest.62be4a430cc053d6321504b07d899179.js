840dd4c6dbb74ba9174408b3417e6b2d16b9af32
slipskid <2773262@gmail.com>
2019-08-07 11:40:28 -0400

    Updates

---
+++ precache-manifest.62be4a430cc053d6321504b07d899179.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d0a9b0ce01f455ff9012891c5a7104",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "f971c0ef947859a61555",
+    "url": "/random-quote-machine/static/js/2.d3524de2.chunk.js"
+  },
+  {
+    "revision": "221471dcfd3d9e0ec7b4",
+    "url": "/random-quote-machine/static/js/main.892cd8fd.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
