15001cd99344e17ed8452799b6e8cec41fa57e67
jonathanhhoward <2773262@gmail.com>
2020-09-11 15:29:47 -0400

    Refactor randomIndex

--- src/getRandomQuote.js
+++ src/getRandomQuote.js
@@ -1,13 +1,10 @@
 import quotes from './quotes'
 
-function getRandomIndex (arrayLength) {
-  return Math.floor(Math.random() * arrayLength)
-}
-
 function getRandomQuote (prevQuote) {
   let nextQuote
   do {
-    nextQuote = quotes[getRandomIndex(quotes.length)]
+    const randomIndex = Math.floor(Math.random() * quotes.length)
+    nextQuote = quotes[randomIndex]
   }
   while (nextQuote.text === prevQuote.text)
   return nextQuote
