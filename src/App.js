32d91e0afcffba48ae50c0540958180337f8c5e4
jonathanhhoward <2773262@gmail.com>
2020-09-11 11:21:18 -0500

    Convert to functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import DisplayQuote from './DisplayQuote'
@@ -7,43 +7,35 @@ import TweetQuote from './TweetQuote'
 import getRandomIndex from './getRandomIndex'
 import quotes from './quotes'
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { quote: {} }
-    this.handleClick = this.handleClick.bind(this)
+function App () {
+  const [quote, setQuote] = useState({})
+
+  useEffect(() => getRandomQuote(), [])
+
+  function handleClick () {
+    getRandomQuote()
   }
 
-  componentDidMount () {
-    this.getRandomQuote()
-  }
-
-  handleClick () {
-    this.getRandomQuote()
-  }
-
-  getRandomQuote () {
-    this.setState(state => {
+  function getRandomQuote () {
+    setQuote(prevQuote => {
       let nextQuote
       do {
         nextQuote = quotes[getRandomIndex(quotes.length)]
       }
-      while (nextQuote.text === state.quote.text)
-      return { quote: nextQuote }
+      while (nextQuote.text === prevQuote.text)
+      return nextQuote
     })
   }
 
-  render () {
-    return (
-      <Container className="align-items-center d-flex vh-100" id="quote-box">
-        <Jumbotron className="m-0 w-100">
-          <DisplayQuote quote={this.state.quote}/>
-          <GetQuote onClick={this.handleClick}/>
-          <TweetQuote quote={this.state.quote}/>
-        </Jumbotron>
-      </Container>
-    )
-  }
+  return (
+    <Container className="align-items-center d-flex vh-100" id="quote-box">
+      <Jumbotron className="m-0 w-100">
+        <DisplayQuote quote={quote}/>
+        <GetQuote onClick={handleClick}/>
+        <TweetQuote quote={quote}/>
+      </Jumbotron>
+    </Container>
+  )
 }
 
 export default App
